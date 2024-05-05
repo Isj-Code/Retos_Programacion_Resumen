e9f0c31b298fd4935661961c1078c0bf7168e8fe
Isj-Code <isj.code@gmail.com>
2024-05-05 10:40:24 +0200

    Ejercicios 05 a 08

---
+++ 08/Program.cs
@@ -0,0 +1,16 @@
+﻿var Uno = new ClaseEjemplo();
+Uno.Saludo("Pepito");
+Uno.Saludo();
+
+
+public class ClaseEjemplo
+{
+    private string saludo = "Hola, ";
+    public string MiNombre { get; set; }
+    public string Edad { get; private set; }
+
+    public void Saludo(string? name = "Anónimo")
+    {
+        Console.WriteLine($"{saludo}{name}");
+    }
+}
